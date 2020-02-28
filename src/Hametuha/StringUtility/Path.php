880cce9b0b002ce69cce7d6b4470f987d2975780
fumikito <guy@hametuha.com>
2020-02-28 18:25:36 +0900

    #1 Add edge case for customized mu-plugins and plugins directory.

--- src/Hametuha/StringUtility/Path.php
+++ src/Hametuha/StringUtility/Path.php
@@ -13,20 +13,27 @@ trait Path {
 	/**
 	 * Convert local file path to URL.
 	 *
-	 * This function is similar to `get_styelsheet_directory_uri()`
-	 * or `plugin_dir_url()`, but works wherever library is.
+	 * This function is similar to `get_stylesheet_directory_uri()`
+	 * or `plugin_dir_url()`, but works wherever the library is.
 	 *
 	 * @param string $path File path in document root.
 	 * @return string URL.
 	 */
 	public function path_to_url( $path ) {
-		if ( false !== strpos( $path, WP_CONTENT_DIR ) ) {
+		if ( false !== strpos( $path, WP_PLUGIN_DIR ) ) {
+			// This is in plugin dir.
+			return str_replace( WP_PLUGIN_DIR, WP_PLUGIN_URL, $path );
+		} elseif ( false !== strpos( $path, WPMU_PLUGIN_DIR ) ) {
+			// This is in mu-plugins dir.
+			return str_replace( WPMU_PLUGIN_DIR, WPMU_PLUGIN_URL, $path );
+		} elseif ( false !== strpos( $path, WP_CONTENT_DIR ) ) {
+			// This is in wp-content dir.
 			return str_replace( WP_CONTENT_DIR, WP_CONTENT_URL, $path );
 		} elseif ( false !== strpos( $path, ABSPATH ) ) {
+			// Convert from ABSPATH.
 			return str_replace( ABSPATH, home_url( '/' ), $path );
 		} else {
 			return $path;
 		}
 	}
-	
 }
--- tests/test-path.php
+++ tests/test-path.php
@@ -12,14 +12,26 @@ class PathTest extends \PHPUnit\Framework\TestCase {
 	
 	protected function setUp() {
 		define( 'ABSPATH', '/var/www/wordpress/' );
+		define( 'WP_CONTENT_DIR', '/var/www/wordpress/contents' );
+		define( 'WP_CONTENT_URL', 'https://example.com/contents' );
+		define( 'WP_PLUGIN_DIR', '/var/www/wordpress/plugins' );
+		define( 'WP_PLUGIN_URL', 'https://example.com/plugins' );
+		define( 'WPMU_PLUGIN_DIR', '/var/www/wordpress/mu' );
+		define( 'WPMU_PLUGIN_URL', 'https://example.com/mu' );
 		$this->path = new HametuhaPathImplementor();
 	}
 	
-	
 	/**
-	 * Test path convertor
+	 * Test path converter.
 	 */
 	public function testPathConverter() {
-		$this->assertEquals( $this->path->path_to_url( '/var/www/wordpress/hoge' ), 'https://example.com/hoge' );
+		// ABSPATH
+		$this->assertEquals( $this->path->path_to_url( '/var/www/wordpress/hoge' ), 'https://example.com/hoge', 'Root directory.' );
+		// In plugins dir.
+		$this->assertEquals( $this->path->path_to_url( '/var/www/wordpress/plugins/my-plugin' ), 'https://example.com/plugins/my-plugin', 'Plugin changed.' );
+		// In mu-plugins dir.
+		$this->assertEquals( $this->path->path_to_url( '/var/www/wordpress/mu/mu-plugin.jpg' ), 'https://example.com/mu/mu-plugin.jpg', 'MU plugins.' );
+		// In themes dir.
+		$this->assertEquals( $this->path->path_to_url( '/var/www/wordpress/contents/themes/style.css' ), 'https://example.com/contents/themes/style.css', 'For theme directory.' );
 	}
 }
